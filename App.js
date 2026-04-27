b1b4b39673f92715c65b7a5f7586ff03887f1ce1
Matheus Gomes Leitão <matheus.g.leitao@aluno.senai.br>
2026-04-27 11:07:21 -0300

    feat: Arruma screens

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-
+// Importando todas as suas telas da pasta src/screens
 import Curiosidades from './src/screens/Curiosidades';
 import OutrosLivros from './src/screens/OutrosLivros';
+import Questoes from './src/screens/Questoes';
+import Equipe from './src/screens/Equipe';
+import VideoAula from './src/screens/VideoAula';
+import DicasVestibular from './src/screens/DicasVestibular';
 
-export default function App() {
-    return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
